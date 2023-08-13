9a65c98516ad9c700b904c0aa89b5b2ba1521d78
ciscomsk <kuznetzoff@gmail.com>
2023-08-13 18:31:45 +0300

    рисование квадрата

--- src/main/java/ru/otus/mike/learn/l_2/MainApp.java
+++ src/main/java/ru/otus/mike/learn/l_2/MainApp.java
@@ -3,5 +3,8 @@ package ru.otus.mike.learn.l_2;
 public class MainApp {
     public static void main(String[] args) {
         System.out.println("Hello World!");
+        System.out.println("---");
+        System.out.println("| |");
+        System.out.println("---");
     }
 }
